adc3f603243480273c30e6efcd45e2a3abd3b8af
Pavan Siligam <pavankumar.siligam@awi.de>
2024-02-16 14:01:20 +0100

    adapted new config to remote checksums function

--- ptool/remotechecksums.py
+++ ptool/remotechecksums.py
@@ -2,39 +2,51 @@ import os
 import time
 import paramiko
 import ptool
-from . import conf
+from . import configure
+
+try:
+    C = configure.Config()
+    C.init_from_user_config()
+except FileNotFoundError:
+    exit()
 
 
-def get_ssh(
-    name: str = None, host: str = None, user: str = None, identityfile: str = None
-):
-    ssh_config = paramiko.SSHConfig()
-    if name:
-        with open(os.path.expanduser("~/.ssh/config")) as fid:
-            ssh_config.parse(fid)
-        c = ssh_config.lookup(name)
-        assert "identityfile" in c, "Valid entry requires identityfile"
-    else:
-        assert host, "host value is required"
-        assert user, "user value is required"
-        assert identityfile, "path to identityfile is required"
-        txt = f"""Host {host}
-        User {user}
-        IdentityFile {identityfile}
-        """
-        sc = ssh_config.from_text(txt)
-        c = sc.lookup(host)
-    C = {}
-    C["hostname"] = c["hostname"]
-    C["username"] = c["user"]
-    C["key_filename"] = c["identityfile"]
+def get_ssh(site: str = None):
+    """
+    Creates a ssh connection object to remote host.
+    `site` must be defined in the config file.
+    
+    Example:
+    >>> import ptool.configure
+    >>> C = ptool.configure.Config()
+    >>> hostname = 'levante'
+    >>> hostname in C
+    True
+    >>> 'undefined_hostname' in C
+    False
+    """
+    host = C[site]['host']
+    user = C[site]['user']
+    identityfile = C.rc['ssh_keys'][site]
+    assert host, "host value is required"
+    assert user, "user value is required"
+    assert identityfile, "path to identityfile is required"
+    params = {}
+    params["hostname"] = host
+    params["username"] = user
+    params["key_filename"] = identityfile
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(**C)
+    ssh.connect(**params)
     return ssh
 
 
 def ensure_checksum_script(ssh):
+    "Ensures `checksums.py` script availability on remote machine"
+    # TODO
+    # Just checking the existance checksums.py file may not be sufficient
+    # Ensuring the latest version of this file on the remote machine is desired
+    #
     csfile = "checksums.py"
     cspath = os.path.join(os.path.dirname(ptool.__file__), csfile)
     i, o, e = ssh.exec_command("ls ~")
@@ -46,28 +58,33 @@ def ensure_checksum_script(ssh):
             scp.put(cspath, csfile)
 
 
-def get_checksum(host, pool, identityfile):
-    stime = time.time()
-    c = conf.get(host)
-    pyexec = c.get("python_executable")
-    user, hostname = c.get("host").split("@")
-    cc = c.get("pool").get(pool)
-    path = cc.get("path")
-    outfile = cc.get("outfile")
-    ignore = ",".join(cc.get("ignore"))
+def get_checksum(site: str, pool: str):
+    """Computes checksums for the given `poolname` on remote host
+
+    `site`: host-name as defined in the config file (example: levante)
+    `pool`: pool-name as defined in the config file (example: fesom2)
+
+    Returns
+    """
+    pyexe = C[site]['python_executable']
+    host = C[site]['host']
+    _pool = C[site]['pool'][pool]
+    path = _pool['path']
+    outfile = _pool['outfile']
+    ignore = ",".join(_pool.get("ignore"))
     print(f"found config for {pool} on {host}")
-    print(f"    pyexec={pyexec}")
+    print(f"    pyexec={pyexe}")
     print(f"    path={path}")
     print(f"    outfile={outfile}")
     print(f"    ignore={ignore}")
-    print(f"creating ssh connection to {hostname}")
-    ssh = get_ssh(host=hostname, user=user, identityfile=identityfile)
+    print(f"creating ssh connection to {site}")
+    stime = time.time()
+    ssh = get_ssh(site)
     ensure_checksum_script(ssh)
     i, o, e = ssh.exec_command("echo $HOME")
     homedir = o.read().decode().strip().rstrip("/")
     OUTFILE = outfile.replace("~", homedir)
-    # cmd = f"~/cs/bin/python -m checksums {path} --outfile {OUTFILE}"
-    cmd = f"{pyexec} checksums.py {path} --outfile {OUTFILE} --ignore {ignore}"
+    cmd = f"{pyexe} checksums.py {path} --outfile {OUTFILE} --ignore {ignore}"
     print(f"calculating checksum for pool {pool}")
     i, o, e = ssh.exec_command(cmd, get_pty=True)
     for line in iter(lambda: o.read(512).decode("utf-8", "ignore"), ""):
