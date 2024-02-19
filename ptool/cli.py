234955974d8127a2a6caff9d300cb0f6abb6796f
Pavan Siligam <pavankumar.siligam@awi.de>
2024-02-19 23:01:30 +0100

    Life without 'config'. Only Essentials. Minimalism.

--- ptool/cli.py
+++ ptool/cli.py
@@ -1,13 +1,8 @@
 import os
 import uuid
 import click
-import yaml
 import pathlib
 from pprint import pprint
-
-from concurrent.futures import ProcessPoolExecutor
-from . import utils
-from . import configure
 
 
 disclaimer = """
@@ -23,108 +18,14 @@ disclaimer = """
 """
 
 
-def process_slurm_directives(d):
-    result = []
-    for key, val in d.items():
-        if key.strip().startswith("--"):
-            if val.strip():
-                s = f"#SBATCH {key}={val}"
-            else:
-                s = f"#SBATCH {key}"
-        else:
-            if val.strip():
-                s = f"#SBATCH {key} {val}"
-            else:
-                s = f"#SBATCH {key}"
-        result.append(s)
-    result = "\n".join(result)
-    return result
-
-
 @click.group()
 def cli():
     pass
-
-'''
-@cli.command()
-@click.option(
-    "--all",
-    "showall",
-    is_flag=True,
-    default=False,
-    help="Gets the whole config in the absence of arguments.",
-)
-@click.argument("site", required=False, default="")
-@click.argument("pool", required=False, default="")
-def config(showall, site, pool):
-    "Shows config information for a given site"
-    if not site:
-        if showall:
-            print(yaml.dump(conf, default_flow_style=False))
-        else:
-            print(f"Possible values for SITE: {','.join(sites)}")
-            print(f"Possible values for POOL: {','.join(pools)}")
-        return
-    if site not in sites:
-        raise ValueError(f"Valid choices for Site: {sites}")
-    if not pool:
-        c = conf[site]
-        print(yaml.dump(c, default_flow_style=False))
-        return
-    elif pool not in pools:
-        raise ValueError(f"Valid choices for Pool: {pools}")
-    c = {}
-    c[site] = conf[site]
-    c[site]["pool"] = {pool: conf[site]["pool"][pool]}
-    print(yaml.dump(c, default_flow_style=False))
-    return
-'''
-
-@cli.command()
-@click.option("-f", "--filename", help="name of the run script")
-@click.argument("site", required=True)
-@click.argument("pool", required=True)
-def runscript(filename, site, pool):
-    "Job script to calculate checksums via slurm scheduler"
-    try:
-        C = configure.Config()
-        C.init_from_user_config()
-    except FileNotFoundError:
-        exit()
-    c = C[site]
-    c_pool = c["pool"][pool]
-    c_slurm = c["slurm"]
-    c_ignore = ",".join(c_pool["ignore"]) if c_pool["ignore"] else ""
-    c_pyexec = c["python_executable"]
-    c_conf_str = yaml.dump(c_pool, default_flow_style=True, width=float("inf")).replace(
-        "\n", ""
-    )
-    slurm_directives = process_slurm_directives(c_slurm)
-    content = f"""#!/bin/bash
-
-{slurm_directives}
-
-{c_pyexec} checksums.py {c_pool['path']} --outfile {c_pool['outfile']} --ignore {c_ignore!r} 
-"""
-    if not filename:
-        filename = f"{pool}_{site}.sh"
-    f = pathlib.Path(filename)
-    with open(f, "w") as fid:
-        fid.write(content)
-    print(f"created '{f}'.\nsubmit to slurm as 'sbatch {f}' on {site}")
-    return
 
 
 @cli.command()
 @click.option(
     "-o", "--outfile", type=click.File("w"), default="-", help="file to write results"
-)
-@click.option(
-    "--format",
-    "fileformat",
-    type=click.Choice(["csv", "yaml"]),
-    default="csv",
-    help="output format (default: csv)",
 )
 @click.option(
     "--fullpath",
@@ -142,7 +43,7 @@ def runscript(filename, site, pool):
 )
 @click.argument("left", required=True, type=click.Path())
 @click.argument("right", required=True, type=click.Path())
-def compare(outfile, fileformat, fullpath, ignore, threshold, left, right):
+def compare(outfile, fullpath, ignore, threshold, left, right):
     """Compare csv files containing checksum to infer the status of data
     in these data pools. The results include, synced files at both HPC sites. unsynced files.
     directory mapping of synced files. filename mis-matches.
@@ -151,19 +52,14 @@ def compare(outfile, fileformat, fullpath, ignore, threshold, left, right):
 
     RIGHT: similar file as LEFT but from different HPC site for the same project.
     """
-    from .analyse import read_csv, compare_compact, compare_directory_view
+    from .analyse import read_csv, compare_compact
 
     ld, ld_dups = read_csv(left, ignore=ignore)
     lr, lr_dups = read_csv(right, ignore=ignore)
-    if fileformat == "yaml":
-        d = compare_directory_view(ld, lr, fullpath=fullpath)
-        click.echo(f"Writing results as yaml to file {outfile.name}")
-        yaml.dump(d, outfile, sort_keys=False)
-        return
     columns = "rpath"
     if fullpath:
         columns = "fpath"
-    res = compare_compact(ld, lr, columns=columns, threshold=threshold, relabel=True)
+    res = compare_compact(ld, lr, columns=columns, threshold=threshold, relabel=False)
     if "stdout" in outfile.name:
         click.echo(res)
     else:
@@ -239,32 +135,75 @@ def sanitise(host, path):
     show_default=True,
     help="minumin value to satisfy valid association",
 )
+@click.option(
+    "-l",
+    "--lefthost",
+    default="",
+    help="username@host prefix to the path for left file",
+)
+@click.option(
+    "-r",
+    "--righthost",
+    default="",
+    help="username@host prefix to the path for right file",
+)
 @click.argument("left", required=True, type=click.Path())
 @click.argument("right", required=True, type=click.Path())
-def prepare_rsync(ignore, Flag, threshold, left, right):
-    """Prepares rsync commands for the transfer."""
+def prepare_rsync(ignore, Flag, threshold, lefthost, righthost, left, right):
+    """Prepares rsync commands for the transfer.
+
+    Denpending on where data needs to pushed or pulled, provide
+    either `--lefthost` or `--righthost` information to prefix that path.
+
+    Note: when Albedo system is invloved, run this command on Albedo and
+    provide the other host information as Albedo can not be reached from
+    other machines.
+
+    Examples that WORK:
+
+    # commands executed on Albedo (i.e., we are on Albedo)
+
+    1. sync data: Levante -> Albedo
+
+       ptool prepare-rsync --lefthost user@levante.dkrz.de checksum_levante_fesom2.csv checksum_albedo_fesom2.csv
+
+    2. sync data: Albedo -> Levante
+
+       ptool prepare-rsync --righthost user@levante.dkrz.de checksum_albedo_fesom2.csv checksum_levante_fesom2.csv
+
+    Examples that FAIL:
+
+    # commands executed on Levante (i.e., we are on Levante)
+
+    1. sync data: Levante -> Albedo
+
+       ptool prepare-rsync --righthost user@albedo0.dmawi.de checksum_levante_fesom2.csv checksum_albedo_fesom2.csv
+
+       will produce rsync commands as follows:
+
+       rsync /some/path/on/levante user@albedo0.dmawi.de:/some/path/on/albedo
+
+       Although syntactically correct command, it fails as Albedo is not reachable from other machines
+
+    2. sync data: Albedo -> Levante
+
+       ptool prepare-rsync --lefthost user@albedo0.dmawi.de checksum_albedo_fesom2.csv checksum_levante_fesom2.csv
+    """
     if Flag == "both":
         Flag = {"unique", "modified_latest_left", "modified_latest_right"}
     elif Flag == "modified":
         Flag = {"modified_latest_left", "modified_latest_right"}
     else:
-        Flag = {"unique", }
+        Flag = {
+            "unique",
+        }
 
     from .analyse import read_csv, compare, compare_compact, directory_map, merge
 
-    try:
-        C = configure.Config()
-        C.init_from_user_config()
-    except FileNotFoundError:
-        exit()
     ld, ld_dups = read_csv(left, ignore=ignore)
     rd, rd_dups = read_csv(right, ignore=ignore)
-    _, left_pool, left_site = os.path.basename(left).split("_")
-    left_site, _ = os.path.splitext(left_site)
-    _, right_pool, right_site = os.path.basename(right).split("_")
-    right_site, _ = os.path.splitext(right_site)
-    left_host = f"{C[left_site]['user']}@{C[left_site]['host']}"
-    right_host = f"{C[right_site]['user']}@{C[right_site]['host']}"
+    left_host = lefthost
+    right_host = righthost
     dm = dict(directory_map(merge(ld, rd)).values)
     c = compare(ld, rd, threshold=threshold)
     fmap = {}
@@ -297,14 +236,35 @@ def prepare_rsync(ignore, Flag, threshold, left, right):
                 sync = f"rsync -av --files-from=flist/{fid} {sanitise(left_host, prefix_left)}{rparent_left}/ {sanitise(right_host, prefix_right)}{rparent_right}/"
             syncs.append(f"\n# {name}")
             syncs.append(sync)
+    flist = ["# file list per directory\n"]
+    flist.append(
+        """
+if [ ! -d flist ]; then
+    mkdir flist
+fi
+"""
+    )
+    flist_add = flist.append
+    for name, fnames in fmap.items():
+        flist_add("\n")
+        flist_add(f"cat > flist/{name} << EOF")
+        flist.extend(fnames)
+        flist_add("EOF")
+    flist = "\n".join(flist)
+    syncs.insert(2, flist)
+    # cleanup
+    # names = " ".join(fmap)
+    syncs.append("\n#cleanup")
+    syncs.append("rm -rf flist")
+    # syncs.append(f"rm {names}")
     syncs = "\n".join(syncs)
-    os.makedirs("flist", exist_ok=True)
+    # os.makedirs("flist", exist_ok=True)
     with open("sync_cmd.sh", "w") as fid:
         fid.writelines(syncs)
-    for name, fnames in fmap.items():
-        with open(f"flist/{name}", "w") as fid:
-            fnames = "\n".join(fnames)
-            fid.writelines(fnames)
+    # for name, fnames in fmap.items():
+    #    with open(f"flist/{name}", "w") as fid:
+    #        fnames = "\n".join(fnames)
+    #        fid.writelines(fnames)
     print("Created sync_cmd.sh")
 
 
@@ -328,39 +288,7 @@ def checksums(path, outfile, ignore, drop_hidden_files):
     from . import checksums
 
     path = os.path.expanduser(path)
-    pool = ProcessPoolExecutor(max_workers=os.cpu_count())
-    checksums.main(pool, path, outfile, ignore, drop_hidden_files)
-
-
-@cli.command()
-@click.argument("site")
-@click.argument("pool")
-def checksums_remote(site, pool):
-    """Calculates imohash checksum on remote pool.
-
-    SITE name of the HPC site.
-    POOL name of the pool (example fesom2)
-    These values are defined in the config file.
-    """
-    from . import remotechecksums
-
-    remotechecksums.get_checksum(site, pool)
-
-
-@cli.command()
-@click.option(
-    "-s",
-    "--ssh-key-test",
-    is_flag=True,
-    default=False,
-    help="Tests ssh keys"
-)
-def config(ssh_key_test):
-    config = configure.Config()
-    if ssh_key_test:
-        config.test_ssh_connection()
-    else:
-        config.all()
+    checksums.main(path, outfile, ignore, drop_hidden_files)
 
 
 if __name__ == "__main__":
